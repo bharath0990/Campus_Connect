6634bf3006be706b225b1a023e82624d66c76826
P. Bharath Kumar Reddy <padakantibharath82@gmail.com>
2026-06-15 23:29:33 +0530

    Fix typo in splash_screen.dart import in main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'services/services.dart';
-import 'views/s  plash_screen.dart';
+import 'views/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
